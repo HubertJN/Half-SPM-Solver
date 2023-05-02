f6d089b1e808b6caa5848a2b272cee1eb26172ac
VGFletcher <125897513+VGFletcher@users.noreply.github.com>
2023-05-02 01:23:08 +0100

    Add files via upload

--- input_output_netcdf.f90
+++ input_output_netcdf.f90
@@ -83,32 +83,47 @@ contains
     end if
   end subroutine assign_real
 
-  !This subroutine opens the netcdf file with name (file_name), reads the input values, writes them to global variables, and closes the file
-  subroutine import_input(file_name)
+  !This subroutine writes the integer 2D array (var), to the variable named (var_name), in the netcdf file with id (file_id), at position (it)
+  !This should be used to write integer arrays (var), to a variable (var_name), with an infinite dimension, (var_len x undefined)
+  subroutine assign_exp_int(var_name, var, file_id, it)
     implicit none
 
-    character(len=*), intent(in) :: file_name
+    character(len=*),    intent(in)    :: var_name
+    integer(kind=int32), intent(in)    :: file_id, it
     
-    integer(kind=int32)          :: ierr, file_id, var_id
-    
-    ierr = nf90_open(file_name, NF90_NOWRITE, file_id)
+    integer(kind=int32), intent(in)    :: var(:,:)
+
+    integer(kind=int32)                :: var_id, ierr
+
+    ierr = nf90_inq_varid(file_id, var_name, var_id)
+    if (ierr /= 0) print*, var_name
     call error_check(ierr)
 
-    call assign_int('sim_steps', sim_steps, file_id, 'r')
-    call assign_int('out_steps', out_steps, file_id, 'r')
-    call assign_real('temp',          temp, file_id, 'r')
-    call assign_real('rad',            rad, file_id, 'r')
-    call assign_real('thick',        thick, file_id, 'r')
-    call assign_real('rr_coef',    rr_coef, file_id, 'r')
-    call assign_real('dif_coef',  dif_coef, file_id, 'r')
-    call assign_real('init_c',      init_c, file_id, 'r')
-    call assign_real('max_c',        max_c, file_id, 'r')
-    call assign_real('c_rate',      c_rate, file_id, 'r')
-    
-    ierr = nf90_close(file_id)
+    ierr = nf90_put_var(file_id, var_id, var, (/1,it/))
     call error_check(ierr)
+
+  end subroutine assign_exp_int
+
+  !This subroutine writes the real 2D array (var), to the variable named (var_name), in the netcdf file with id (file_id), at position (it)
+  !This should be used to write real arrays (var), to a variable (var_name), with an infinite dimension, (var_len x undefined)
+  subroutine assign_exp_real(var_name, var, file_id, it)
+    implicit none
+
+    character(len=*),    intent(in)    :: var_name
+    integer(kind=int32), intent(in)    :: file_id, it
     
-  end subroutine import_input
+    real(kind=real64), intent(in)      :: var(:,:)
+
+    integer(kind=int32)                :: var_id, ierr
+
+    ierr = nf90_inq_varid(file_id, var_name, var_id)
+    if (ierr /= 0) print*, var_name
+    call error_check(ierr)
+
+    ierr = nf90_put_var(file_id, var_id, var, (/1,it/))
+    call error_check(ierr)
+
+  end subroutine assign_exp_real
 
   !This subroutine creates a single value variable called (var_name), with data type (var_typ (f90_int or f90_double)), in a netcdf file with id (file_id)
   !You can optionally prescribe units to the variable (units)
@@ -183,89 +198,6 @@ contains
 
   end subroutine create_exp_var
 
-  !This subroutine writes the integer 2D array (var), to the variable named (var_name), in the netcdf file with id (file_id), at position (it)
-  !This should be used to write integer arrays (var), to a variable (var_name), with an infinite dimension, (var_len x undefined)
-  subroutine assign_exp_int(var_name, var, file_id, it)
-    implicit none
-
-    character(len=*),    intent(in)    :: var_name
-    integer(kind=int32), intent(in)    :: file_id, it
-    
-    integer(kind=int32), intent(in)    :: var(:,:)
-
-    integer(kind=int32)                :: var_id, ierr
-
-    ierr = nf90_inq_varid(file_id, var_name, var_id)
-    if (ierr /= 0) print*, var_name
-    call error_check(ierr)
-
-    ierr = nf90_put_var(file_id, var_id, var, (/1,it/))
-    call error_check(ierr)
-
-  end subroutine assign_exp_int
-
-  !This subroutine writes the real 2D array (var), to the variable named (var_name), in the netcdf file with id (file_id), at position (it)
-  !This should be used to write real arrays (var), to a variable (var_name), with an infinite dimension, (var_len x undefined)
-  subroutine assign_exp_real(var_name, var, file_id, it)
-    implicit none
-
-    character(len=*),    intent(in)    :: var_name
-    integer(kind=int32), intent(in)    :: file_id, it
-    
-    real(kind=real64), intent(in)      :: var(:,:)
-
-    integer(kind=int32)                :: var_id, ierr
-
-    ierr = nf90_inq_varid(file_id, var_name, var_id)
-    if (ierr /= 0) print*, var_name
-    call error_check(ierr)
-
-    ierr = nf90_put_var(file_id, var_id, var, (/1,it/))
-    call error_check(ierr)
-
-  end subroutine assign_exp_real
-
-  !This subroutine creates a netcdf file named (file_name), initiates input variables, saves input variables, and initiates variables to write output data to
-  subroutine initiate_file(file_name)
-    implicit none
-    
-    character(len=*), intent(in) :: file_name
-
-    integer(kind=int32)          :: ierr, file_id
-    
-    ierr = nf90_create(file_name, nf90_clobber, file_id)
-    call error_check(ierr)
-
-    call create_sing_var('sim_steps', nf90_int,    file_id)
-    call create_sing_var('out_steps', nf90_int,    file_id)
-    call create_sing_var('temp',      nf90_double, file_id, 'K')
-    call create_sing_var('rad',       nf90_double, file_id, 'm')
-    call create_sing_var('thick',     nf90_double, file_id, 'm')
-    call create_sing_var('rr_coef',   nf90_double, file_id)
-    call create_sing_var('dif_coef',  nf90_double, file_id)
-    call create_sing_var('init_c',    nf90_double, file_id)
-    call create_sing_var('max_c',     nf90_double, file_id)
-    call create_sing_var('c_rate',    nf90_double, file_id)
-
-    ierr = nf90_enddef(file_id)
-    call error_check(ierr)
-
-    call assign_int('sim_steps', sim_steps, file_id, 'w')
-    call assign_int('out_steps', out_steps, file_id, 'w')
-    call assign_real('temp',          temp, file_id, 'w')
-    call assign_real('rad',            rad, file_id, 'w')
-    call assign_real('thick',        thick, file_id, 'w')
-    call assign_real('rr_coef',    rr_coef, file_id, 'w')
-    call assign_real('dif_coef',  dif_coef, file_id, 'w')
-    call assign_real('init_c',      init_c, file_id, 'w')
-    call assign_real('max_c',        max_c, file_id, 'w')
-    call assign_real('c_rate',      c_rate, file_id, 'w')
-
-    ierr = nf90_close(file_id)
-    call error_check(ierr)
-
-  end subroutine initiate_file
-
   !This subroutine opens an existing netcdf file named (file_name), and writes a single integer value (var) to an existing variable named (var_name)
   !if (act='new'), this assumes the variable doesn't already exist and will create a single integer variable called (var_name), with units (units), and write the single integer (var) to this variable
   subroutine save_int(var_name, var, file_name, units, act)
@@ -320,16 +252,25 @@ contains
 
   !This subroutine opens an existing netcdf file named (file_name), and writes the integer 2D array (var), to the variable named (var_name) at position (it)
   !This should be used to write integer arrays (var), to a variable (var_name), with an infinite dimension, (var_len x undefined)
-  subroutine save_exp_int(var_name, var, file_name, it)
+  !if (act='new'), this assumes the variable doesn't already exist and will create an integer 2D array variable called (var_name), with shape (var_len x undefined), with units (units),...
+  !...and then write the 2D integer array (var) to this variable
+  subroutine save_exp_int(var_name, var, file_name, it, units, act)
     implicit none
 
-    character(len=*),    intent(in)         :: var_name, file_name
-    integer(kind=int32), intent(in)         :: var(:,:), it
+    character(len=*),    intent(in)           :: var_name, file_name
+    integer(kind=int32), intent(in)           :: var(:,:), it
+    character(len=*),    intent(in), optional :: units, act
 
-    integer(kind=int32)                     :: ierr, file_id
+    integer(kind=int32)                       :: ierr, file_id, var_len(2)
 
     ierr = nf90_open(file_name, NF90_WRITE, file_id)
     call error_check(ierr)
+
+    if (act == 'new') then
+       var_len = shape(var)
+       
+       call create_exp_var(var_name, nf90_int, var_len(1), file_id, units, act='add')
+    end if
 
     call assign_exp_int(var_name, var, file_id, it)
 
@@ -340,17 +281,26 @@ contains
 
   !This subroutine opens an existing netcdf file named (file_name), and writes the real 2D array (var), to the variable named (var_name) at position (it)
   !This should be used to write real arrays (var), to a variable (var_name), with an infinite dimension, (var_len x undefined)
-  subroutine save_exp_real(var_name, var, file_name, it)
+  !if (act='new'), this assumes the variable doesn't already exist and will create a real 2D array variable called (var_name), with shape (var_len x undefined), with units (units),...
+  !...and then write the 2D real array (var) to this variable
+  subroutine save_exp_real(var_name, var, file_name, it, units, act)
     implicit none
 
-    character(len=*),    intent(in)         :: var_name, file_name
-    real(kind=real64),   intent(in)         :: var(:,:)
-    integer(kind=int32), intent(in)         :: it
-
-    integer(kind=int32)                     :: ierr, file_id
+    character(len=*),    intent(in)           :: var_name, file_name
+    real(kind=real64),   intent(in)           :: var(:,:)
+    integer(kind=int32), intent(in)           :: it
+    character(len=*),    intent(in), optional :: units, act
+    
+    integer(kind=int32)                       :: ierr, file_id, var_len(2)
 
     ierr = nf90_open(file_name, NF90_WRITE, file_id)
     call error_check(ierr)
+
+    if (act == 'new') then
+       var_len = shape(var)
+       
+       call create_exp_var(var_name, nf90_double, var_len(1), file_id, units, act='add')
+    end if
 
     call assign_exp_real(var_name, var, file_id, it)
 
@@ -358,5 +308,75 @@ contains
     call error_check(ierr)
     
   end subroutine save_exp_real
+
+  !This subroutine opens the netcdf file with name (file_name), reads the input values, writes them to global variables, and closes the file
+  subroutine import_input(file_name)
+    implicit none
+
+    character(len=*), intent(in) :: file_name
+    
+    integer(kind=int32)          :: ierr, file_id, var_id
+    
+    ierr = nf90_open(file_name, NF90_NOWRITE, file_id)
+    call error_check(ierr)
+
+    call assign_int('sim_steps', sim_steps, file_id, 'r')
+    call assign_int('out_steps', out_steps, file_id, 'r')
+    call assign_real('temp',          temp, file_id, 'r')
+    call assign_real('rad',            rad, file_id, 'r')
+    call assign_real('thick',        thick, file_id, 'r')
+    call assign_real('rr_coef',    rr_coef, file_id, 'r')
+    call assign_real('dif_coef',  dif_coef, file_id, 'r')
+    call assign_real('init_c',      init_c, file_id, 'r')
+    call assign_real('max_c',        max_c, file_id, 'r')
+    call assign_real('c_rate',      c_rate, file_id, 'r')
+    
+    ierr = nf90_close(file_id)
+    call error_check(ierr)
+    
+  end subroutine import_input
+
+  !This subroutine creates a netcdf file named (file_name), initiates input variables, saves input variables, and initiates variables to write output data to
+  subroutine initiate_file(file_name)
+    implicit none
+    
+    character(len=*), intent(in) :: file_name
+
+    integer(kind=int32)          :: ierr, file_id
+    
+    ierr = nf90_create(file_name, nf90_clobber, file_id)
+    call error_check(ierr)
+
+    call create_sing_var('sim_steps', nf90_int,    file_id)
+    call create_sing_var('out_steps', nf90_int,    file_id)
+    call create_sing_var('temp',      nf90_double, file_id, 'K')
+    call create_sing_var('rad',       nf90_double, file_id, 'm')
+    call create_sing_var('thick',     nf90_double, file_id, 'm')
+    call create_sing_var('rr_coef',   nf90_double, file_id)
+    call create_sing_var('dif_coef',  nf90_double, file_id)
+    call create_sing_var('init_c',    nf90_double, file_id)
+    call create_sing_var('max_c',     nf90_double, file_id)
+    call create_sing_var('c_rate',    nf90_double, file_id)
+
+    ierr = nf90_enddef(file_id)
+    call error_check(ierr)
+
+    call assign_int('sim_steps', sim_steps, file_id, 'w')
+    call assign_int('out_steps', out_steps, file_id, 'w')
+    call assign_real('temp',          temp, file_id, 'w')
+    call assign_real('rad',            rad, file_id, 'w')
+    call assign_real('thick',        thick, file_id, 'w')
+    call assign_real('rr_coef',    rr_coef, file_id, 'w')
+    call assign_real('dif_coef',  dif_coef, file_id, 'w')
+    call assign_real('init_c',      init_c, file_id, 'w')
+    call assign_real('max_c',        max_c, file_id, 'w')
+    call assign_real('c_rate',      c_rate, file_id, 'w')
+
+    ierr = nf90_close(file_id)
+    call error_check(ierr)
+
+  end subroutine initiate_file
   
 end module input_output_netcdf
+
+!Need to modify assign_exp... to read as well for the checkpoint system, otherwise complete.

5cad9276b3f05503452993655ae1d7751550c1b1
anson1515 <119423562+anson1515@users.noreply.github.com>
2023-05-03 17:54:35 +0100

    Version 1.1
    Additional functions to pde.f90
    New main program main.f90
    - Pointers for optimisation (do we really?)
    - Need to add voltage line (cc. Vince)

---
+++ main.f90
@@ -0,0 +1,98 @@
+PROGRAM main
+
+  USE ISO_FORTRAN_ENV
+  USE pde_solver
+  USE input_output_netcdf
+  
+  IMPLICIT NONE
+  
+  !Overload function for voltage calculation
+  INTERFACE volt_calc
+    MODULE PROCEDURE volt_scalar
+    MODULE PROCEDURE volt_array
+  END INTERFACE volt_calc
+  
+  !TODO: main program:
+  !test functions
+  !essential variables
+  !Read input
+  !initialise/load from checkpoint
+  !NO NEED TO CALCULATE STABILITY CRITERION BECAUSE CRANK-NICHOLSON IS UNCONDITIONALLY STABLE
+  !Mesh
+  !Parallel toggle
+  !Time loop (propagate+voltage+write)
+  
+  !temporary
+  CHARACTER(LEN=7) :: loadname = "test.nc", outname = "tout.nc"
+  REAL(REAL64) :: a, iapp, flux_param
+  REAL(REAL64), DIMENSION(:,:), ALLOCATABLE :: conc
+  REAL(REAL64), DIMENSION(:), ALLOCATABLE :: c_tmp, volt
+  INTEGER :: i, j, quo
+  LOGICAL :: test, volt_do                                        !test - default or not; voltage calcs or not
+  
+  !Import file and initialise mesh
+  CALL import_input(loadname,test)
+  
+  !allocate
+  IF (ALLOCATED(conc)) THEN
+    DEALLOCATE(conc)
+  END IF
+  
+  IF (ALLOCATED(c_tmp)) THEN
+    DEALLOCATE(c_tmp)
+  END IF
+  
+  IF (ALLOCATED(volt)) THEN
+    DEALLOCATE(volt)
+  END IF
+  
+  ALLOCATE(conc(space_steps,out_steps))
+  ALLOCATE(c_tmp(space_steps))
+  
+  conc = -1.0_REAL64
+  volt_do = .FALSE.
+  quo = CEILING(sim_steps/out_steps) 
+  
+  !initial conditions and derived parameters
+  c_tmp = init_c
+  a = 3.0_REAL64*vol_perc/(100.0_REAL64*rad)
+  iapp = c_rate*dt/area
+  flux_param = iapp/(a*farad*thick)
+  
+  
+  IF (.NOT. volt_do) THEN
+    
+    !loop to evolve
+    DO i = 1:quo
+      DO j = 1:out_steps
+        c_tmp = crank_nicholson(rad,dif_coef,flux_param,dt,c_tmp)
+        conc(:,j) = c_tmp
+      END DO
+      
+      !save
+      CALL save_exp_real('conc',conc,outname,(i-1)*out_steps+1)
+    END DO
+    
+  ELSE
+    !include voltage calcs
+    ALLOCATE(volt(out_steps))
+    
+    DO i = 1:quo
+      DO j = 1:out_steps
+        c_tmp = crank_nicholson(rad,dif_coef,flux_param,dt,c_tmp)
+        conc(:,j) = c_tmp
+      END DO
+      
+      !Actual calculation
+      volt = volt_calc(conc(space_steps,:), gas_con, temp, farad, iapp, a, thick, rr_coef, max_c)
+      
+      !save
+      CALL save_exp_real('conc',conc,outname,(i-1)*out_steps+1)
+      CALL save_exp_real('volt',volt,outname,(i-1)*out_steps+1)
+    END DO
+    
+  END IF
+    
+
+
+END PROGRAM main
--- pde.f90
+++ pde.f90
@@ -25,7 +25,7 @@ MODULE pde_solver
     REAL(REAL64), DIMENSION(:,:), ALLOCATABLE :: B                              !evolution matrix
     REAL(REAL64), DIMENSION(:), ALLOCATABLE :: AL, A, AU, rhs		        !lhs and rhs of equation
     REAL(REAL64) :: dr, ri, ai                                                  !spacestep, current radius, parameter
-    INTEGER :: i, j                                                             !loop variables
+    INTEGER :: i                                                                !loop variables
     INTEGER :: n, info                                                          !size of array and dgtsv info
     
     !Get size of input array
@@ -124,15 +124,120 @@ MODULE pde_solver
     !Error handling - crude at this stage
     IF (info < 0) THEN
       PRINT *, 'Argument number', -info, 'in dgtsv is illegal' 
-      STOP
+      ERROR STOP
     ELSE IF (info > 0) THEN
       PRINT *, 'Determinant of matrix A is zero in Ax=b'
-      STOP
+      ERROR STOP
     END IF
     
     !Assign solution to output matrix
     crank_nicholson = rhs
     
   END FUNCTION crank_nicholson
+  
+  
+  FUNCTION U_scalar(x)
+  
+    !Accepts SCALAR x - stoichiometry
+    !Outputs U(c)
+    
+    REAL(REAL64), INTENT(IN) :: x
+    REAL(REAL64), INTENT(OUT) :: U_scalar
+    
+    !U+ implemented (U- can be implemented later if needed)
+    U_scalar = -0.8090_REAL64*x + 4.4875_REAL64 - 0.0428_REAL64*TANH(18.5138_REAL64*(x-0.5542_REAL64)) &
+               -17.7326_REAL64*TANH(15.7890_REAL64*(x-0.3117_REAL64)) & 
+               +17.5842_REAL64*TANH(15.9308_REAL64*(x-0.3120_REAL64))
+                 
+  END FUNCTION U_scalar
+  
+  
+  FUNCTION U_arr(x)
+    
+    !Accepts 1D ARRAY x - stoichiometry
+    !Outputs U(c) as 1D array
+    
+    REAL(REAL64), DIMENSION(:), INTENT(IN) :: x
+    REAL(REAL64), DIMENSION(:), ALLOCATABLE, INTENT(OUT) :: U_arr
+    INTEGER :: size_arr
+    
+    IF(ALLOCATED(U_arr)) THEN
+      DEALLOCATE(U_arr)
+    END IF
+    
+    size_arr = SIZE(x)
+    
+    ALLOCATE(U_arr(size_arr))
+    
+    !U+ implemented (U- can be implemented later if needed)
+    U_arr = -0.8090_REAL64*x + 4.4875_REAL64 - 0.0428_REAL64*TANH(18.5138_REAL64*(x-0.5542_REAL64)) &
+            -17.7326_REAL64*TANH(15.7890_REAL64*(x-0.3117_REAL64)) & 
+            +17.5842_REAL64*TANH(15.9308_REAL64*(x-0.3120_REAL64))
+    
+  END FUNCTION U_arr
+  
+  
+  FUNCTION volt_scalar(cin, Rg, T, F, iapp, a, L, K, cmax)
+  
+    !Calculates scalar voltage when given 
+    !a SCALAR INPUT of concentration
+    !and ESSENTIAL PARAMETERS
+    
+    !Rg = 'gas_con'
+    !T = 'temp'
+    !F = 'farad'
+    !L = 'thick'
+    !K = 'rr_coef'
+    !cmax = 'max_c'
+    
+    !TODO: Neaten up (prevent writing so many params, maybe incorporate into module itself?)
+    REAL(REAL64), INTENT(IN) :: cin, Rg, T, F, iapp, a, L, K, cmax
+    REAL(REAL64) :: arsinh
+    REAL(REAL64), INTENT(OUT) :: volt_scalar 
+    
+    !Calculates arsinh part
+    arsinh = F*K*SQRT((cin/cmax)*(1.0_REAL64 - cin/cmax))          !jc
+    arsinh = iapp/(a*L*arsinh)                                     !argument of arsinh
+    arsinh = LOG(arsinh + SQRT(arsinh**2.0_REAL64 + 1.0_REAL64))   !arsinh
+    
+    volt_scalar = U_scalar(cin/cmax) - (2.0_REAL64*Rg*T/F)*arsinh
+    
+  END FUNCTION volt_scalar
+  
+  
+  FUNCTION volt_array(arrin, Rg, T, F, iapp, a, L, K, cmax)
+    
+    !Calculates an array of voltages when given 
+    !an ARRAY INPUT of concentrations
+    !and ESSENTIAL PARAMETERS
+    
+    !TODO: Neaten up (prevent writing so many params, maybe incorporate into module itself?)
+    REAL(REAL64), DIMENSION(:), INTENT(IN) :: arrin
+    REAL(REAL64), INTENT(IN) :: Rg, T, F, iapp, a, L, K, cmax
+    REAL(REAL64), DIMENSION(:), ALLOCATABLE :: arsinh
+    REAL(REAL64), DIMENSION(:), ALLOCATABLE, INTENT(OUT) :: volt_array 
+    INTEGER :: size_arr
+    
+    IF (ALLOCATED(volt_array)) THEN
+      DEALLOCATE(volt_array)
+    END IF
+    
+    IF (ALLOCATED(arsinh)) THEN
+      DEALLOCATE(arsinh)
+    END IF
+    
+    size_arr = SIZE(arrin)
+    
+    ALLOCATE(volt_array(size_arr))
+    ALLOCATE(arsinh(size_arr))
+    
+    !Calculates arsinh part
+    arsinh = F*K*SQRT((arrin/cmax)*(1.0_REAL64 - arrin/cmax))       !jc
+    arsinh = iapp/(a*L*arsinh)                                      !argument of arsinh
+    arsinh = LOG(arsinh + SQRT(arsinh**2.0_REAL64 + 1.0_REAL64))    !arsinh
+    
+    volt_array = U_arr(arrin/cmax) - (2.0_REAL64*Rg*T/F)*arsinh
+    
+  END FUNCTION volt_array
     
 END MODULE pde_solver

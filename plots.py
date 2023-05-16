95d71806ea952bb347df5340b1b0bd1339d3c905
Chantal Baer <Chantal.Baer@warwick.ac.uk>
2023-05-16 10:41:16 +0100

    improved speed of animation, added time cursor to voltage plot and included option to not write voltage data

--- plots.py
+++ plots.py
@@ -10,16 +10,18 @@ from matplotlib.animation import FuncAnimation
 
 # reading in the NetCDF data from SP_output.nc
 dat = NC.Dataset("SP_output.nc", "r", format="NETCDF4")
+do_volt = dat['do_volt'][:][0]
 
-#creating figure and axes
-fig = plt.figure()
-ax1 = plt.subplot2grid((2,2),(0,0))
-ax2 = plt.subplot2grid((2,2),(0,1))
-ax3 = plt.subplot2grid((2,2),(1,0), colspan=2)
-
-# animation of the concentration data 
-# the main parts of the code were taken from:
-# https://brushingupscience.com/2016/06/21/matplotlib-animations-the-easy-way/
+#creating figure and axes dependent on whether voltage data is to be written or not
+if do_volt == 1:
+	fig = plt.figure(figsize=(10,8))
+	ax1 = plt.subplot2grid((2,2),(0,0))
+	ax2 = plt.subplot2grid((2,2),(0,1))
+	ax3 = plt.subplot2grid((2,2),(1,0), colspan=2)
+else:
+	fig = plt.figure(figsize=(10,4))
+	ax1 = plt.subplot2grid((1,2),(0,0))
+	ax2 = plt.subplot2grid((1,2),(0,1))
 
 
 # Subplot 1 - Animtion of lithium concentration 
@@ -31,17 +33,22 @@ rad = dat['rad'][:][0]*10**(6)
 time_axis = np.linspace(0, t_steps-1, t_steps-1).astype(int)
 x_axis = np.linspace(0, rad, r_steps, endpoint=True)
 
-intervaltime = dat['dt'][:][0]*10**3   #time between each animations step in ms
+# animation of the concentration data 
+# the main parts of the  animation code were taken from:
+# https://brushingupscience.com/2016/06/21/matplotlib-animations-the-easy-way/
+
+intervaltime = 0.5   #time between each animations step in ms
 
 # plotting the first graph which is a 2D line of lithium ion concentration
 # with respect to the particle radius
-line = ax1.plot(x_axis, c[0,:])[0]
+line, = ax1.plot(x_axis, c[0])
 
 # function evolving the animation, which is called each frame 
 # the x-axis is the particle radius, so only the x-axis data
 # i.e. the lithium concantration changes with simulation time
 def animate(t):
     line.set_ydata(c[t,:])
+    return line,
 
 # creating the animation using matplotlib's FunAnimation
 # the arguments to be passed:
@@ -49,7 +56,8 @@ def animate(t):
 # function evolving the animation - animate(t)
 # interval time between each timesetep - intervaltime
 # frames, the array to iterate over - time_axis
-animation = FuncAnimation(fig, animate, interval=intervaltime, frames=time_axis)
+# blit - blitting set to true
+animation = FuncAnimation(fig, animate, interval=intervaltime, frames=time_axis, blit=True)
 
 # customise the graph with axes labels, major and minor ticks, labels etc.
 ax1.set_xlabel('particle radius [$\mu m$]', size=8)
@@ -66,26 +74,26 @@ ax1.set_title('Lithium concentration', size=10, pad=15.0)
 
 # Subplot 2 - pcolorplot of concentration data
 
-# a lot of the data is the same as in subplot 1
+# most of the required data is the same as in subplot 1
+# get the discretisation steps of the radius
 dr = rad/(r_steps-1)
  
-#rainbow colormap, values over the range are black, under goes white
-colour = cm.get_cmap('gist_rainbow').copy()
+# reversed virdis colormap, values over the range are black, under goes white
+colour = cm.get_cmap('viridis_r').copy()
 colour.set_under(color='w')
 colour.set_over(color='k')
 max_colour = np.max(c)
 min_colour = np.min(c)
 ticklist = np.linspace(c.min(), c.max(), 6)
 
-#create finer grid for plotting
-gridsize = 200
-x = np.linspace(-rad,rad,gridsize,endpoint=True)
-y = np.linspace(-rad,rad,gridsize,endpoint=True)
+#create grid for plotting
+x = np.linspace(-rad,rad,r_steps,endpoint=True)
+y = np.linspace(-rad,rad,r_steps,endpoint=True)
 
-#form 2D array of radii
+# form 2D array of radii
 z = np.array([np.sqrt(i*i+j*j) for j in y for i in x])
 
-#form concentration values; give negative concentration for colourmap if out of range - goes white
+# form concentration values; give negative concentration for colourmap if out of range - goes white
 for idx, r in enumerate(z):
 	if (r <= rad):
 		ri = np.rint(r/dr).astype(np.int32)
@@ -94,7 +102,7 @@ for idx, r in enumerate(z):
 		z[idx] = -1
 
 # reshape to form 2D array for pcolor
-Z = z.reshape(gridsize,gridsize)
+Z = z.reshape(r_steps,r_steps)
 
 # form meshgrid and plot using pcolor
 X, Y = np.meshgrid(x,y)
@@ -118,8 +126,8 @@ cbar.ax.yaxis.set_major_formatter(StrMethodFormatter("{x:.10f}"))
 # works generally the same as for subplot 1
 # the concentration data for each timestep is created as in the pcolor plot above
 # while iterating through the time dimension of the concentratoin data
-# the resulting 2D array is flattened in the end to a 1D array that can be used by FuncAnimation
-# the inteval and frames are the same as in subplot 1
+# the resulting 2D array is flattened in the end to an 1D array that can be used by FuncAnimation
+# the inteval and frames are the same as in subplot 1 and blitting is set to true
 def animate_pcol(t):
     z =  np.array([np.sqrt(i*i+j*j) for j in y for i in x])
     for idx, r in enumerate(z):
@@ -128,29 +136,42 @@ def animate_pcol(t):
 		    z[idx] = c[t,:][ri]
 	    else:
 		    z[idx] = -1
-    Z = z.reshape(gridsize,gridsize)
+    Z = z.reshape(r_steps,r_steps)
     cplot.set_array(Z.flatten())
-animation_pcol = FuncAnimation(fig, animate_pcol, interval=intervaltime, frames=time_axis)
+    return cplot,
 
+animation_pcol = FuncAnimation(fig, animate_pcol, interval=intervaltime, frames=time_axis, blit=True)
 
-# Subplot 3 - plot of voltage output
+# Subplot 3 - plot of voltage output if do_volt set to true
+if do_volt == 1:
 
-# accessing the voltage data and setting the number of time steps as x-axis
-volt = dat['volt'][:][:,0]
-time = np.linspace(0, t_steps, t_steps).astype(int)
+	# accessing the voltage data and setting the number of time steps as x-axis
+	volt = dat['volt'][:][:,0]
+	time = np.linspace(0, t_steps, t_steps)
 
-# plotting the 2D graph and customising it
-ax3.plot(time, volt)
-ax3.set_xlabel('time [s]', size=8)
-ax3.xaxis.set_minor_locator(AutoMinorLocator())
-ax3.tick_params(axis='x', labelsize=7)
-ax3.set_ylabel('Voltage [V]', size=8)
-ax3.yaxis.set_minor_locator(AutoMinorLocator())
-ax3.tick_params(axis='y', labelsize=7)
-ax3.ticklabel_format(axis='both', style="sci", useMathText=True)
-ax3.xaxis.offsetText.set_fontsize(7)
-ax3.yaxis.offsetText.set_fontsize(7)
-ax3.set_title('Voltage output', size=10, pad=15.0)
+	# plotting the 2D graph and customising it
+	ax3.plot(time, volt)
+	ax3.set_xlabel('time [s]', size=8)
+	ax3.xaxis.set_minor_locator(AutoMinorLocator())
+	ax3.tick_params(axis='x', labelsize=7)
+	ax3.set_ylabel('Voltage [V]', size=8)
+	ax3.yaxis.set_minor_locator(AutoMinorLocator())
+	ax3.tick_params(axis='y', labelsize=7)
+	ax3.ticklabel_format(axis='both', style="sci", useMathText=True)
+	ax3.xaxis.offsetText.set_fontsize(7)
+	ax3.yaxis.offsetText.set_fontsize(7)
+	ax3.set_title('Voltage output', size=10, pad=15.0)
+
+	# animation of an curser that moves with the time axis with the same interval and frames
+	# as the other two animations
+	vl = ax3.axvline(time[0], color='black', linestyle=':')
+	ax3.set_xlim()
+
+	def animate_t_bar(t):
+		vl.set_xdata(time[t])
+		return vl,
+
+	animation_t_bar = FuncAnimation(fig, animate_t_bar, interval=intervaltime, frames=time_axis, blit=True)
 
 plt.draw()
 plt.tight_layout()

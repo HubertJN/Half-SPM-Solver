831c87611d90d65c4d1a58b041e9fc660572b2c0
VGFletcher <125897513+VGFletcher@users.noreply.github.com>
2023-05-17 13:09:40 +0100

    Add files via upload

--- input_output_netcdf.f90
+++ input_output_netcdf.f90
@@ -10,7 +10,7 @@ module input_output_netcdf
   real(kind=Real64), parameter :: farad = 96485.3321233100184_DP !C/mol
   real(kind=Real64), parameter :: gas_con = 8.31446261815324_DP !J/(K.mol)
   
-  real(kind=Real64)            :: temp, rad, thick, rr_coef, dif_coef, iapp !, c_rate, area
+  real(kind=Real64)            :: temp, rad, thick, rr_coef, dif_coef, iapp
   real(kind=Real64)            :: init_c, max_c, dt, vol_per, final_time
   integer(kind=int32)          :: sim_steps, out_steps, space_steps, tot_steps
   integer(kind=int32)          :: output_id, check_id

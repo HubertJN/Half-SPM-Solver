ce437317986bfa7439870e7750bf51eb65d5c2f5
Maria Radova <56935802+7radians@users.noreply.github.com>
2023-05-19 15:19:48 +0100

    Add files via upload

--- datafitPde.f90
+++ datafitPde.f90
@@ -1,4 +1,4 @@
-MODULE datafitPde_solver
+MODULE datafitPde
   !> @file datafitPde.f90
   !! @brief Program file for solving the diffusion equation and voltage calculation, called for data fitting. 
   !!
@@ -51,19 +51,20 @@ CONTAINS
   !! The rescaled diffusion coefficient given by: \f$ \frac{D}{R^2} \f$  
   FUNCTION crank_nicholson(n, totalTime, D, R, volPer, iapp, F, L, Rg, T, K, maxCon, c0, timeArr) RESULT(voltArray)
 
-    INTEGER(INT32), INTENT(IN)                                      :: n, totalTime
-    REAL(REAL64), DIMENSION(:,:), ALLOCATABLE                       :: A, B
-    REAL(REAL64),   DIMENSION(:,:), ALLOCATABLE                     :: A_mod
-    REAL(REAL64), DIMENSION(n), INTENT(IN)                          :: c0, timeArr
-    REAL(REAL64), DIMENSION(:),   ALLOCATABLE                       :: c_cur, rhs
-    REAL(REAL64)                                                    :: U_scalar
-    REAL(REAL64), DIMENSION(totalTime), intent(out)                 :: voltArray
+    INTEGER(4), INTENT(IN)                                      :: n, totalTime
+    REAL(8), DIMENSION(:,:), ALLOCATABLE                       :: A, B
+    REAL(8),   DIMENSION(:,:), ALLOCATABLE                     :: A_mod
+    REAL(8), DIMENSION(:), INTENT(IN)                          :: timeArr
+    REAL(8), DIMENSION(n), INTENT(IN)                          :: c0
+    REAL(8), DIMENSION(:),   ALLOCATABLE                       :: c_cur, rhs
+    REAL(8)                                                    :: U_scalar
+    REAL(8), DIMENSION(totalTime)                :: voltArray
     
-    REAL(REAL64), INTENT(IN)                                        :: D, R, volPer, iapp, F, L, Rg, T, K, maxCon
-    REAL(REAL64)                                                    :: ai, ri, num, fluxParam, dt, dr, &
+    REAL(8), INTENT(IN)                                        :: D, R, volPer, iapp, F, L, Rg, T, K, maxCon
+    REAL(8)                                                    :: ai, ri, num, fluxParam, dt, dr, &
                                                                        rhsConst, voltConIal, voltConRtf, modD 
-    REAL(REAL64)                                                    :: arsinh, div_const !div_const is the stoichiometry.
-    INTEGER(INT32)                                                  :: i, info, totalSim, time
+    REAL(8)                                                    :: arsinh, div_const !div_const is the stoichiometry.
+    INTEGER(4)                                                  :: i, info, totalSim, time
     INTEGER, DIMENSION(n)                                           :: ipiv
 
     ALLOCATE(A(n,n))
@@ -78,54 +79,54 @@ CONTAINS
     totalSim = SIZE(timeArr)
 
     dt = timeArr(2) - timeArr(1)
-    dr = 1.0_REAL64/(REAL(n-1, KIND=REAL64))
+    dr = 1.0d0/(REAL(n-1, 8))
 
-    num = 3.0_REAL64*volPer/(100.0_REAL64*R)
+    num = 3.0d0*volPer/(100.0d0*R)
     modD = D/(R**2)
     
     fluxParam = iapp/(num*F*L)
     voltConIal = iapp/(num*L)
-    voltConRtf = (2.0_REAL64*Rg*T)/F
+    voltConRtf = (2.0d0*Rg*T)/F
 
-    A = 0.0_REAL64
-    B = 0.0_REAL64
+    A = 0.0d0
+    B = 0.0d0
 
-    ai = dt*modD/(2.0_REAL64*dr*dr)
+    ai = dt*modD/(2.0d0*dr*dr)
 
     DO i=2,n-1
       !current radius and dimensionless parameter ai
       ri = REAL((i-1),KIND=REAL64)*dr
       
       !LHS
-      A(i,i-1) = ai*(-1.0_REAL64 + dr/ri)
-      A(i,i) = 1.0_REAL64 + 2.0_REAL64*ai
-      A(i,i+1) = ai*(-1.0_REAL64 - dr/ri)
+      A(i,i-1) = ai*(-1.0d0 + dr/ri)
+      A(i,i) = 1.0d0 + 2.0d0*ai
+      A(i,i+1) = ai*(-1.0d0 - dr/ri)
       
       !RHS
-      B(i,i-1) = ai*(1.0_REAL64 - dr/ri)
-      B(i,i) = 1.0_REAL64 - 2.0_REAL64*ai
-      B(i,i+1) = ai*(1.0_REAL64 + dr/ri)      
+      B(i,i-1) = ai*(1.0d0 - dr/ri)
+      B(i,i) = 1.0d0 - 2.0d0*ai
+      B(i,i+1) = ai*(1.0d0 + dr/ri)      
    END DO
 
    !smooth boundary conditions at r=0
     
-    A(1,1) = 1.0_REAL64 + 2.0_REAL64*ai
-    A(1,2) = -2.0_REAL64*ai
-    B(1,1) = 1.0_REAL64 - 2.0_REAL64*ai
-    B(1,2) = 2.0_REAL64*ai
+    A(1,1) = 1.0d0 + 2.0d0*ai
+    A(1,2) = -2.0d0*ai
+    B(1,1) = 1.0d0 - 2.0d0*ai
+    B(1,2) = 2.0d0*ai
     
     !flux boundary conditions
-    A(n,n-1) = -2.0_REAL64*ai
-    A(n,n) = 1.0_REAL64 + 2.0_REAL64*ai
-    B(n,n-1) = 2.0_REAL64*ai
-    B(n,n) = 1.0_REAL64 - 2.0_REAL64*ai
+    A(n,n-1) = -2.0d0*ai
+    A(n,n) = 1.0d0 + 2.0d0*ai
+    B(n,n-1) = 2.0d0*ai
+    B(n,n) = 1.0d0 - 2.0d0*ai
 
-    rhsConst = 2.0_REAL64*fluxParam*(dt + dt/dr)
+    rhsConst = 2.0d0*fluxParam*(dt + dt/dr)
     !------------------------------------------------------------------------------------------
     !-------------------------------Run the solver over the time array: -----------------------
     ALLOCATE(c_cur(n))
 
-    DO time = 1, totalSim 
+    DO time = 1, totalSim
     
       IF(ALLOCATED(rhs)) THEN
         DEALLOCATE(rhs)
@@ -140,8 +141,8 @@ CONTAINS
       ALLOCATE(rhs(n))
       ALLOCATE(A_mod(n,n))
       
-      rhs = 0.0_REAL64
-      c_cur = 0.0_REAL64
+      rhs = 0.0d0
+      c_cur = 0.0d0
       
       A_mod = A
       !Use initial concentration at first timestep, otherwise use previour timestep concentration. 
@@ -171,15 +172,15 @@ CONTAINS
       !--------------------------Calculate stoichiometry: -----------------------------------------------
       div_const = c_cur(n)/maxCon
       !------------------------U+ implemented (U- can be implemented later if needed): -----------------
-      U_scalar = -0.8090_REAL64*div_const + 4.4875_REAL64 - 0.0428_REAL64*TANH(18.5138_REAL64*(div_const-0.5542_REAL64)) &
-              -17.7326_REAL64*TANH(15.7890_REAL64*(div_const-0.3117_REAL64)) & 
-              +17.5842_REAL64*TANH(15.9308_REAL64*(div_const-0.3120_REAL64))
+      U_scalar = -0.8090d0*div_const + 4.4875d0 - 0.0428d0*TANH(18.5138d0*(div_const-0.5542d0)) &
+              -17.7326d0*TANH(15.7890d0*(div_const-0.3117d0)) & 
+              +17.5842d0*TANH(15.9308d0*(div_const-0.3120d0))
       !----------------------------Calculate arsinh part: ----------------------------------------------
       arsinh = F*K*SQRT(div_const - (div_const**2))
       arsinh = voltConIal/arsinh
       arsinh = ASINH(arsinh)
       !----------------------------Calculate the voltage: ----------------------------------------------
-      voltArray(time-1) = U_scalar - (voltConRtf*arsinh)
+      voltArray(time) = U_scalar - (voltConRtf*arsinh)
     END DO
 
     !DEALLOCATE(c_cur); DEALLOCATE(A_mod); DEALLOCATE(rhs); DEALLOCATE(A);
@@ -187,4 +188,4 @@ CONTAINS
     
   END FUNCTION crank_nicholson
 
-END MODULE datafitPde_solver
+END MODULE datafitPde

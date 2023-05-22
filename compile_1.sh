7db4c411e163e1e563c8be960c295c2eecc00817
Maria Radova <56935802+7radians@users.noreply.github.com>
2023-05-22 15:09:38 +0100

    Add files via upload

---
+++ compile_1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+chmod u+r+x compile.sh if permission denied
+
+sudo apt install python3.10-venv
+python3 -m venv venv
+source venv/bin/activate
+vim requirements.txt - names of mods
+pip install requirements.txt 
+pip install pandas
+pip install matplotlib
+pip freeze list >> requirements.txt - add all existing modules to requirements
+#deactivate
+
+
+
+f2pyfiles="datafitPde.f90"
+#rm *.o *.mod
+gfortran -c -W $f2pyfiles -llapack 
+f2py -c -m f90_src $f2pyfiles -llapack #--f2cmap .kind_map
+
+
